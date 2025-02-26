46d0a188c9bf9ef76cc21edd475282bf4ad07674
Simocnini <77299123+AlessioSmn@users.noreply.github.com>
2025-02-26 12:09:17 +0100

    doc: comments modified, variable name changes

--- src/CodeGenerator.vhd
+++ src/CodeGenerator.vhd
@@ -1,29 +1,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Generates convolutional code symbols.
+--    It processes the current input, past inputs and past codewords
+--    It applies masks to determine which values contribute to the next codeword
 entity CodeGenerator is
       generic (
-            -- Input memory of the convolutional code
+            -- Input memory of the convolutional code (without considering the current input)
             InputMemory: positive := 8;
 
-            -- State memory of the convolutional code
-            StateMemory: positive := 8
+            -- Codeword memory of the convolutional code
+            CodewordMemory: positive := 8
       );
       port(
             -- Current input
             c:    in    std_logic;
-            -- Mask to select if to consider the current input to determine the current state
+            -- Mask to select if to consider the current input to determine the next codeword
             c_m:  in    std_logic;
 
             -- Past inputs array
             i:	in    std_logic_vector(InputMemory - 1 downto 0);
-            -- Mask to select which inputs to consider to determine the current state
+            -- Mask to select which inputs to consider to determine the next codeword
             i_m:  in    std_logic_vector(InputMemory - 1 downto 0);
 
-            -- Past states array
-            s:	in    std_logic_vector(StateMemory - 1 downto 0);
-            -- Mask to select which states to consider to determine the current state
-            s_m:  in    std_logic_vector(StateMemory - 1 downto 0);
+            -- Past codewords array
+            s:	in    std_logic_vector(CodewordMemory - 1 downto 0);
+            -- Mask to select which codewords to consider to determine the next codeword
+            s_m:  in    std_logic_vector(CodewordMemory - 1 downto 0);
 
             -- Code output
             o:	out    std_logic
@@ -34,7 +37,7 @@ architecture beh of CodeGenerator is
 
       -- Derived dimensions
       constant TotalInputMemory : positive := InputMemory + 1;
-      constant AdderDimension : positive := TotalInputMemory + StateMemory;
+      constant AdderDimension : positive := TotalInputMemory + CodewordMemory;
 
       component Mask is
             generic (
@@ -57,31 +60,27 @@ architecture beh of CodeGenerator is
             );
       end component;
 
-      -- Signal composed by c and i
+      -- Concatenated signal containing the current input ('c') and past inputs ('i')
       signal inputs: std_logic_vector(TotalInputMemory - 1 downto 0);
 
-      -- Signal composed by c_m and i_m
+      -- Concatenated signal containing the mask for the current input ('c_m') and past input masks ('i_m')
       signal input_mask: std_logic_vector(TotalInputMemory - 1 downto 0);
 
-      -- Signal from the input mask to the adder
-      -- signal input_masked_sig: std_logic_vector(TotalInputMemory - 1 downto 0);
-
-      -- Signal from the state mask to the adder
-      -- signal state_masked_sig: std_logic_vector(StateMemory - 1 downto 0);
-
-      -- Signal from the masks to the adder
+      -- Masked signal to be processed by the adder
       signal masked_sig: std_logic_vector(AdderDimension - 1 downto 0);
 
 
 begin
 
+      -- Construct 'inputs' array as previously described
       inputs(0) <= c;
       inputs(TotalInputMemory - 1 downto 1) <= i;
       
+      -- Construct 'inputs_mask' array as previously described
       input_mask(0) <= c_m;
       input_mask(TotalInputMemory - 1 downto 1) <= i_m;
 
-      -- Input mask, also includes the current input
+      -- Mask component, in order to mask the 'inputs' array
       MaskInput: Mask
             generic map(N => TotalInputMemory)
             port map(
@@ -90,22 +89,23 @@ begin
                   o => masked_sig(TotalInputMemory - 1 downto 0)
             );
 
-      -- State mask
-      MaskState: Mask
-            generic map(N => StateMemory)
+      -- Mask component, in order to mask the codeword array ('s')
+      MaskCodeword: Mask
+            generic map(N => CodewordMemory)
             port map(
                   i => s,
                   mask => s_m,
                   o => masked_sig(AdderDimension - 1 downto TotalInputMemory)
             );
 
-      -- Code egenrator
+      -- Generation of the new code symbol
+      -- It computes the XOR of all bits in the 'masked_sig' array
+      -- The ouput of the Adder component is used as the output of the CodeGenerator entity
       CodeAdder: Adder
             generic map(N => AdderDimension)
             port map(
                   a => masked_sig,
                   s => o
             );
-
 
 end architecture;
