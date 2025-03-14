0d590a1af362bf1c90aa3b4a117d4379238bad78
Simocnini <77299123+AlessioSmn@users.noreply.github.com>
2025-03-14 19:51:05 +0100

    fix: tester code fixed

--- modelsim/codeTester/tester.cpp
+++ modelsim/codeTester/tester.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include <time.h>
 #include <random>
 using namespace std;
 
 #define CYCLES 30
 #define INPUT_REG_DIM 4
-#define STATE_REG_DIM 10
+#define STATE_REG_DIM 6
 #define CURRENT_INPUT_NAME "a_k_ext"
-#define EXPECTED_OUTPUT_NAME "o_ext"
+#define EXPECTED_OUTPUT_NAME "c_expected"
+#define CURRENT_MASK_NAME "c_m_ext"
+#define INPUT_MASK_NAME "i_m_ext"
+#define STATE_MASK_NAME "s_m_ext"
 #define CLOCK_PERIOD "clk_period"
 #define RISING_EDGE "rising_edge(clk_ext)"
+
+// Variabile globale per il file di output
+ofstream outputTextFile;
 
 enum InputStreamType{
       AllZeros,
@@ -22,20 +29,20 @@ void getInputStreamTypeName(InputStreamType inputStreamType){
       {
       
       case Random:
-            cout << "Random stream";
+            outputTextFile <<  "\t\t" << "Random stream";
             return;
       
       case AllOnes:
-            cout << "Only ones";
+            outputTextFile <<  "\t\t" << "Only ones";
             return;
 
       case Alternate:
-            cout << "Alternate";
+            outputTextFile <<  "\t\t" << "Alternate";
             return;
 
       default:
       case AllZeros:
-            cout << "Only zeros";
+            outputTextFile <<  "\t\t" << "Only zeros";
             return;
             break;
       }
@@ -112,43 +119,73 @@ bool generateNewState(
 }
 
 void print_input(bool in){
-      cout << CURRENT_INPUT_NAME << " <= '" << (in == 0 ? "0" : "1") << "';" << endl;
+      outputTextFile <<  "\t\t" << CURRENT_INPUT_NAME << " <= '" << (in == 0 ? "0" : "1") << "';" << endl;
 }
 void print_inputStream(bool* inputs){
-      cout << "-- Input: ";
+      outputTextFile <<  "\t\t" << "-- Input: ";
       for(int i = 0; i < CYCLES; i++)
-            cout << (inputs[i] ? "1" : "0");
-      cout << endl;
+            outputTextFile <<  (inputs[i] ? "1-" : "0-");
+      outputTextFile <<  "\t\t" << endl;
+}
+void print_mask(bool* mask, int maskDim, string CodeName, string Name){
+      outputTextFile <<  "\t\t" << "-- " << Name << " Mask: " << endl;
+      outputTextFile <<  "\t\t" << CodeName << " <= \"";
+      for(int i = 0; i < maskDim; i++)
+            outputTextFile <<  (mask[i] ? "1" : "0");
+      outputTextFile << "\";" << endl;
 }
 void print_waitForClockPeriod(){
-      cout << "wait for " << CLOCK_PERIOD << ";"<< endl << endl;
+      outputTextFile <<  "\t\t" << "wait for " << CLOCK_PERIOD << ";"<< endl;
 }
 void print_expectedOutput(bool exptOut){
-      cout << EXPECTED_OUTPUT_NAME << " <= '" << exptOut << "';" << endl;
+      outputTextFile <<  "\t\t" << EXPECTED_OUTPUT_NAME << " <= '" << exptOut << "';" << endl;
+}
+
+void print_register_info(
+      bool *inputReg, bool *inputRegMask, int inputRegDim,
+      bool *stateReg, bool *stateRegMask, int stateRegDim
+){
+      // Prints registers content content
+      outputTextFile <<  "\t\t" << " -- Register states [";
+      for(int i = 0; i < inputRegDim; i++)
+            outputTextFile <<  (inputReg[i] ? "1" : "0");
+      outputTextFile << "] [";
+      for(int i = 0; i < stateRegDim; i++)
+            outputTextFile <<  (stateReg[i] ? "1" : "0");
+      outputTextFile << "]" << endl;
+      
+      // Prints only register entry used with the mask
+      outputTextFile <<  "\t\t" << " -- Register masked [";
+      for(int i = 0; i < inputRegDim; i++)
+            outputTextFile <<  (inputRegMask[i] ? (inputReg[i] ? "1" : "0") : "x");
+      outputTextFile << "] [";
+      for(int i = 0; i < stateRegDim; i++)
+            outputTextFile <<  (stateRegMask[i] ? (stateReg[i] ? "1" : "0") : "x");
+      outputTextFile << "]" << endl;
 }
 
 
-void generateTestBenchText(InputStreamType inputStreamType){
+void generateTestBenchText(InputStreamType inputStreamType, bool currentInputMask, bool *inputMask, bool *stateMask){
 
       bool inputStream[CYCLES];
       bool inputRegister[INPUT_REG_DIM];
       bool stateRegister[STATE_REG_DIM];
 
       // Initialize masks
-      bool currentInputMask = 1;
-      bool inputMask[INPUT_REG_DIM] = {0,0,1,1};
-      bool stateMask[STATE_REG_DIM] = {0,0,0,0,0,0,0,1,0,1};
 
       srand(time(NULL));
 
       generateRandomInputStream(inputStream, inputStreamType);
 
-      cout << "--" << endl;
-      cout << "--" << endl;
-      cout << "-- Input type: ";
+      outputTextFile << endl << endl << endl << endl << endl << "\t\t" << "-- Input type: ";
       getInputStreamTypeName(inputStreamType);
-      cout << endl;
+      outputTextFile <<  "\t\t" << endl;
       print_inputStream(inputStream);
+
+      print_mask(inputMask, INPUT_REG_DIM, INPUT_MASK_NAME, "Input");
+      print_mask(stateMask, STATE_REG_DIM, STATE_MASK_NAME, "Codeword");
+
+      outputTextFile << endl;
 
       // Initialize the two shift registers
       initializeShiftRegister(inputRegister, INPUT_REG_DIM);
@@ -172,22 +209,48 @@ void generateTestBenchText(InputStreamType inputStreamType){
 
             // Print data
             print_input(inputStream[i]);
-            print_expectedOutput(newState);
             print_waitForClockPeriod();
+            print_expectedOutput(newState);
+            outputTextFile << endl;
+            print_register_info(
+                  inputRegister, inputMask, INPUT_REG_DIM,
+                  stateRegister, stateMask, STATE_REG_DIM
+            );
+      }
+}
+void openFile() {
+      string filePath = "C:/intelFPGA/18.1/projects/ConvolutionalCoder/modelsim/codeTester/lines/convcode_tester.txt";
+      outputTextFile.open(filePath, ios::out);
+      if (!outputTextFile.is_open()) {
+            cerr << "Errore nell'aprire il file: " << filePath << endl;
+            exit(1);
       }
 }
 
 int main(){
 
-      // Firstly run 0 and 1
-      generateTestBenchText(AllZeros);
-      generateTestBenchText(AllOnes);
+      // Open the output file
+      openFile();
+
+      // Run 0
+      bool inputMask[INPUT_REG_DIM] = {0,0,1,1};
+      bool stateMask[STATE_REG_DIM] = {1,0,0,1,0,1};
+      generateTestBenchText(AllZeros, 1, inputMask, stateMask);
+
+      // Run 1
+      bool inputMask2[INPUT_REG_DIM] = {1,1,0,0};
+      bool stateMask2[STATE_REG_DIM] = {0,1,1,0,1,0};
+      generateTestBenchText(AllOnes, 1, inputMask2, stateMask2);
 
       // Run alternate
-      generateTestBenchText(Alternate);
+      bool inputMask3[INPUT_REG_DIM] = {1,0,0,1};
+      bool stateMask3[STATE_REG_DIM] = {0,1,0,0,0,1};
+      generateTestBenchText(Alternate, 1, inputMask3, stateMask3);
 
       // Run random
-      generateTestBenchText(Random);
+      bool inputMask4[INPUT_REG_DIM] = {0,1,0,0};
+      bool stateMask4[STATE_REG_DIM] = {0,1,1,0,0,0};
+      generateTestBenchText(Random, 1, inputMask4, stateMask4);
 
       return 0;
 }
