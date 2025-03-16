07839882ca6f9c196ef84c6b806aae5ad2674087
Simocnini <77299123+AlessioSmn@users.noreply.github.com>
2025-03-16 10:51:20 +0100

    feat: register removed from wrapper

--- src/Wrapper_CC.vhd
+++ src/Wrapper_CC.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 -- Convolutional Coder wrapper
--- 1) Resolves all generic instances using the project specifications
--- 2) Puts register on output lines if not present
+--    Resolves all generic instances using the project specifications
 
 entity CC_Wrapper is
       port(
@@ -20,7 +19,7 @@ entity CC_Wrapper is
             a_k_i:      in    std_logic;
 
             -- Current input, output line
-            a_k_o:	out std_logic;
+            a_k_o:	out   std_logic;
 
             -- Flag to signal if data output ('c') is valid
             o_valid:	out	std_logic;
@@ -33,44 +32,34 @@ end entity;
 architecture beh of CC_Wrapper is
 
       -- Convolutional Code Memory dimensions, as per specifications
-      constant InputMemory : positive := 4;
-      constant CodewordMemory : positive := 10;
+      constant InputMemory :        positive := 4;
+      constant CodewordMemory :     positive := 10;
 
       -- Masks for the convolutional code, as per specifications
-      constant CurrentInputMask : std_logic:= '1';
+      constant CurrentInputMask : std_logic := '1';
       constant InputMask : std_logic_vector(InputMemory - 1 downto 0) := "0011"; -- Only a(k-3) and a(k-4)
       constant CodewordMask : std_logic_vector(CodewordMemory - 1 downto 0) := "0000000101";  -- Only c(k-8) and c(k-10)
 
       component ConvolutionalCoder is
+            generic (
+                  InputMemory: positive := 8;
+                  CodewordMemory: positive := 8
+            );
             port(
                   clk:	      in	std_logic;
                   res:	      in	std_logic;
                   i_valid:	in	std_logic;
-                  a_k:	      inout std_logic;
+                  a_k_i:	in    std_logic;
                   c_mask:     in    std_logic;
                   i_mask:     in    std_logic_vector(InputMemory - 1 downto 0);
                   s_mask:     in    std_logic_vector(CodewordMemory - 1 downto 0);
                   o_valid:	out	std_logic;
+                  a_k_o:	out	std_logic;
                   c:	      out   std_logic
             );
       end component;
 
-      component FlipFlopD is
-            port(
-                  clk:	in	std_logic;
-                  res:	in	std_logic;
-                  en:	in	std_logic;
-                  i:	in    std_logic;
-                  o:	out   std_logic
-            );
-      end component;
-
-      signal a_k_sig: std_logic;
-
 begin
-
-      -- Link the in line of the current input to the internal signal
-      a_k_sig <= a_k_i;
 
       -- ConvolutionalCoder component
       convCode: ConvolutionalCoder
@@ -82,22 +71,13 @@ begin
                   clk => clk,
                   res => res,
                   i_valid => i_valid,
-                  a_k => a_k_sig,
+                  a_k_i => a_k_i,
                   c_mask => CurrentInputMask,
                   i_mask => InputMask,
                   s_mask => CodewordMask,
+                  o_valid => o_valid,
+                  a_k_o => a_k_o,
                   c => c
             );
-      
-      -- Output register for a_k (here called a_k_o)
-      akRegister: FlipFlopD
-            port map(
-                  clk => clk,
-                  res => res,
-                  en => i_valid,
-                  i => a_k_i,
-                  o => a_k_o
-            );
-
 
 end architecture;
